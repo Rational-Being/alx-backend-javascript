39268999a0628e83c2b08a2ed38da4ad0515b19b
Rational-Being <overcomer021@gmail.com>
2024-04-22 21:12:53 +0100

    ES6 is really sweet

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
-export default function returnHowManyArguments()_{
-    
+export default function returnHowManyArguments(...noOfArgs) {
+    return noOfArgs.length
 }
---
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -0,0 +1,14 @@
+export default function getSanFranciscoDeescription() {
+    const year = 2017;
+    const budget = {
+        income: '$119,868',
+        gpd: '$154.2 billion',
+        capita: '$178,479',
+    };
+
+    return `As of ${year}, it was the seventh-highest income county in the united States
+    , with a per capita personal income of ${budget.income}. As of 2015, San Francisco
+    proper had a GDP of ${budget.gpd}, and a GDP per capita of ${budget.capita}.`
+
+
+}
