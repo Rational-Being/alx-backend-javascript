40f2ccdf03d35ec89e71ceff7458e8d93547c91a
Rational-Being <overcomer021@gmail.com>
2024-04-23 00:04:29 +0100

    correction

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,3 @@
-export default function getSumofHoods(
-  initialNumber,
-  expansion1989 = 89,
-  expansion2019 = 9,
-) {
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 9,) {
   return initialNumber + expansion1989 + expansion2019;
 }
