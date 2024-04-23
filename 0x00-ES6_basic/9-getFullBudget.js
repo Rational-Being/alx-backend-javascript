9d9edf36e2f85eb266a7d66caef20e9c564b127b
Rational-Being <overcomer021@gmail.com>
2024-04-23 08:31:19 +0100

    ES6 is getting better

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,18 +1,18 @@
 import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
-    const budget = getBudgetObject(income, gdp, capita)
+  const budget = getBudgetObject(income, gdp, capita);
 
-    const fullBudget = {
-        ...budget,
-        getIncomeInDollars(income) {
-            return `$${income}`;
-        },
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
 
-        getIncomeInEuros(income) {
-            return `${income} euros`;
-        },
-    };
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
 
-    return fullBudget
+  return fullBudget;
 }
