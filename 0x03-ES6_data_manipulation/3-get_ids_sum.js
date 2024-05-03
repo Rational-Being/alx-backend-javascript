48e53aae659fc07d757647a471a68428285e6e39
Rational-Being <overcomer021@gmail.com>
2024-05-03 19:40:06 +0100

    lint

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,6 @@
 export default function getStudumentIdsSum(studentArray) {
-  const studentsId = studentArray.reduce((accumulator, currentStudents) => accumulator + currentStudents.id, 0);
+  const studentsId = studentArray.reduce(
+    (accumulator, currentStudents) => accumulator + currentStudents.id, 0,
+  );
   return studentsId;
 }
