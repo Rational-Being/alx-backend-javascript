8a13f0ef7c6be5bc3f16f49ce70e15cda4183f34
rational-being <overcomer021@gmail.com>
2024-06-04 19:58:18 +0100

    small http server using node js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', '23');
+
+  res.writeHead(200);
+
+  res.end('Hello holberton School!');
+});
+
+module.exports = app;
+
+app.listen(1245, '127.0.0.1', () => {});
+module.exports = app;
