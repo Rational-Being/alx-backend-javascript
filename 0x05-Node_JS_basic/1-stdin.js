11d510d1555dc31512bb27f2e2df454898c8eab2
rational-being <overcomer021@gmail.com>
2024-06-04 13:50:46 +0100

    reading file sync with node js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,3 +1,5 @@
+//a program that takes stfin and is executed in the command line
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // if (process.stdin.isTTY) {
@@ -7,7 +9,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 //   });
 // } else {
 process.stdin.on('data', (name) => {
-  process.stdout.write(`Your name is: ${name.toString().trim()}\n`);
+  process.stdout.write(`Your name is: ${name.toString().trim()}`);
   process.exit();
 });
 process.on('exit', () => {
---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,46 @@
+//this is script reads a file synchronusly with nodejs 
+
+const fs = require('fs');
+
+function countStudents(file) {
+  try {
+    const input = fs.readFileSync(file, 'utf-8');
+    const lines = input.toString().split('\n');
+    const header = lines[0].split(',');
+    const students = [];
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const line = lines[i].split(',');
+      if (line.length === header.length) {
+        const student = {};
+
+        for (let j = 0; j < header.length; j += 1) {
+          student[header[j]] = line[j];
+        }
+        students.push(student);
+      }
+    }
+
+    console.log(`Number of students: ${students.length}`);
+
+    const counts = {};
+    const lists = {};
+
+    for (const student of students) {
+      if (!counts[student.field]) {
+        counts[student.field] = 0;
+        lists[student.field] = [];
+      }
+      counts[student.field] += 1;
+      lists[student.field].push(student.firstname);
+    }
+
+    for (const field in counts) {
+      console.log(`Number of students in ${field}: ${counts[field]}. List: ${lists[field].join(', ')}`);
+    }
+  } catch (error) {
+    console.error('Cannot load the batabase');
+  }
+}
+
+module.exports = countStudents;
