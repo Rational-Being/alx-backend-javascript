c0efba16fc6cea8d954b1eabc3eb7baf41b29393
rational-being <overcomer021@gmail.com>
2024-06-04 11:40:41 +0100

    using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,15 +2,15 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (name) => {
-    console.log(`Your name is: ${name.toString}`);
+    process.stdout.write(`Your name is: ${name.toString}`);
     process.exit();
   });
 } else {
   process.stdin.on('data', (name) => {
-    console.log(`Your name is: ${name.toString}`);
+    process.stdout.write(`Your name is: ${name.toString}`);
     process.exit();
   });
   process.on('exit', () => {
-    console.log('This important software is now closing');
+    process.stdout.write('This important software is now closing');
   });
 }
