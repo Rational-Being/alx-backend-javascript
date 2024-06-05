a0359c3502a76e86735d0903d96aa56cf65e37cf
rational-being <overcomer021@gmail.com>
2024-06-05 10:12:07 +0100

    first commit

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,54 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul')
+
+describe('calculateNumber', () => {
+    it('test two rounded numbers', () => {
+        assert.equal(calculateNumber(4.5, 5.4), 10);
+        assert.equal(calculateNumber(5.5, 4.4), 10);
+        assert.equal(calculateNumber(4.0, 5.0), 9);
+        assert.equal(calculateNumber(4, 5.4), 9);
+
+    });
+    
+    it('test floting point', () => {
+        assert.strictEqual(calculateNumber(1.799999, 7.911111), 10);
+    });
+    
+    // it('', () => {
+    //     assert.(calculateNumber(), );
+    // });
+    
+    // it('', () => {
+    //     assert.(calculateNumber(), );
+    // });
+    
+    // it('', () => {
+    //     assert.(calculateNumber(), );
+    // });
+    
+    // it('', () => {
+    //     assert.(calculateNumber(), );
+    // });
+    
+    // it('', () => {
+    //     assert.(calculateNumber(), );
+    // });
+    
+    // it('', () => {
+    //     assert.(calculateNumber(), );
+    // });
+    
+    // it('', () => {
+    //     assert.(calculateNumber(), );
+    // });
+    
+    // it('', () => {
+    //     assert.(calculateNumber(), );
+    // });
+    
+    // it('', () => {
+    //     assert.(calculateNumber(), );
+    // });
+    
+    
+})
