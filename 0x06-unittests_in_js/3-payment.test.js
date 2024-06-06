5c66f960f661a9afd9b97cb75f6a4b8d5a9e3f80
rational-being <overcomer021@gmail.com>
2024-06-06 14:50:42 +0100

    now, this is what i am saying

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,10 +2,9 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils')
-const { it, describe } = require('mocha')
 
 describe('sendPaymentRequestToApi', () => {
-  it('thi si the test', () => {
+  it('3 payment test', () => {
     const hello = sinon.spy(Utils);
 
     sendPaymentRequestToApi(100, 20);
