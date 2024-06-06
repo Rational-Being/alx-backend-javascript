39992f577109866843ed83862cc35b64c0a8f947
rational-being <overcomer021@gmail.com>
2024-06-06 15:22:09 +0100

    5-payment.test.js

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,27 +1,27 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./5-payment');
 
 describe('sendPaymentRequestToApi', () => {
   let hello;
 
-  beforeEach(() => {
-    hello = sinon.spy(console, 'log');
-  });
-  
   afterEach(() => {
     expect(hello.calledOnce).to.be.true;
     hello.restore();
   });
 
-  it('logs the total to the console', () => {
-    sendPaymentRequestToApi(100, 20);
-    expect(hello.calledWith('The total is: 10')).to.be.false;
+  beforeEach(() => {
+    hello = sinon.spy(console, 'log');
   });
 
   it('logs the total is 120 to to the console', () => {
     sendPaymentRequestToApi(10, 10);
-    expect(hello.calledWith('The total os: 20')).to.be.true;
+    expect(hello.calledWith('The total is: 20')).to.be.true;
+  });
+
+  it('logs the total to the console', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(hello.calledWith('The total is: 10')).to.be.false;
   });
 
 });
