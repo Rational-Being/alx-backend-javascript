00a958df6a39879354f528018d043307cadc0c6c
rational-being <overcomer021@gmail.com>
2024-06-06 15:49:32 +0100

    8-api

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,15 +2,17 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('API GET testing', () => {
-    const allowed = {
-        API_url: "http://localhost:7865",
-        method: 'GET'
-    };
+   
 
     it('tests for correct response', (done) => {
-        request(allowed, (_, res, body) => {
+        // const allowed = {
+        //     _url: "http://localhost:7865/",
+        //     method: 'GET'
+        // };
+        // request(allowed, (err, res, body) => {
+            request.get("http://localhost:7865/", (_, res, body) => {
             expect(res.statusCode).to.be.below(400);
-            expect(body).to.equal("Welcome to the payment system");
+            expect(body).to.be.equal("Welcome to the payment system");
             done();
         });
     });
